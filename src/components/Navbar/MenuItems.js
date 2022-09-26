8115bd803830580f999d06bde20119c3da8c877b
nmdatar <ndatar18@gmail.com>
2022-09-25 22:23:25 -0400

    initial deletions website

---
+++ src/components/Navbar/MenuItems.js
@@ -0,0 +1,22 @@
+export const MenuItems = [
+    {
+        title: 'Home',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'About',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Resume',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Contact',
+        url: '#',
+        cName: 'nav-links'
+    },
+]
